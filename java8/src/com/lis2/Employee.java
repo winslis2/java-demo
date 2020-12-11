47b73e1f39d2b95d574aae55773769832c117922
kunmingqaq@163.com <kunmingqaq@163.com>
2020-12-11 12:33:27 +0800

    lambda\方法引用\构造器引用

---
+++ java8/src/com/lis2/Employee.java
@@ -0,0 +1,53 @@
+package com.lis2;
+
+import javax.swing.*;
+
+public class Employee {
+    private String name;
+    private Integer id;
+    private Double salary;
+
+
+    public Employee(String name, Integer id, Double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", salary=" + salary +
+                '}';
+    }
+}
--- java8/src/com/lis2/Lambda.java
+++ java8/src/com/lis2/Lambda.java
@@ -2,9 +2,14 @@ package com.lis2;
 
 import org.junit.Test;
 
+import java.io.PrintStream;
+import java.lang.module.ResolutionException;
 import java.util.Comparator;
 import java.util.TreeSet;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Lambda {
     public static void main(String[] args) {
@@ -58,4 +63,72 @@ public class Lambda {
         Integer operate = operate (1, 3, (a, b) -> a + b);
         System.out.println (operate);
     }
+    //JAVA内置四大核心函数式接口
+    //消费型接口 接受一个参数，无返回值
+    @Test
+    public void test6() {
+        Consumer<Integer> consumer = x -> System.out.println (x);
+        consumer.accept (1000);
+
+        Consumer<Integer> consumer1 = System.out::println;//方法引用
+        consumer1.accept (200);
+    }
+
+    //供给型接口  没有参数，一个返回值
+    @Test
+    public void test7() {
+        //获取Employee对象
+        Supplier<Employee> sup = ()-> new Employee ();
+        Employee employee = sup.get ();
+        System.out.println (employee);
+    }
+
+    //函数式接口  一个参数，一个返回值
+    @Test
+    public void test8() {
+        //字符串截取功能
+        Function<String, String> fun = (str)-> str.substring (1, 2);
+        String apply = fun.apply ("123456");
+        System.out.println (apply);
+    }
+
+    //断言型接口  一个参数 返回bool
+    @Test
+    public void test9() {
+        //判断员工的工资是否大于5000
+        Predicate<Employee> predicate = (emp)->{
+            return emp.getSalary () > 5000;
+        };
+        boolean test = predicate.test (new Employee ("lis2",2,5000.1));
+        System.out.println (test);
+    }
+
+    //方法引用：若Lambda方法体中的内容已有方法实现，可以使用方法引用
+    //对象::实例方法
+    @Test
+    public void test10() {
+        PrintStream out = System.out;
+        Consumer<String> con = x ->out.println (x);
+        //Lambda表达实体中调用方法的参数列表和返回值类型和函数式接口中抽象方法保持一致
+        Consumer<String> con1 = out::println;
+    }
+
+    //类::静态方法
+    @Test
+    public void test11() {
+        //注意不要使用Compare
+        Comparator<Integer> com = (x, y) -> Integer.compare (x, y);
+        Comparator<Integer> com1 = Integer::compare;//使用方法引用是不需要参数的
+        com.compare (1,2);
+    }
+
+    //构造器引用
+    @Test
+    public void test12() {
+        Supplier<Employee> supplier = ()-> new Employee ();
+        Employee employee = supplier.get ();
+    }
+
+
+
 }
